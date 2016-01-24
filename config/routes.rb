56704aa6316f03de640d1cf1cf23a0953f349c7c
yatindrarao <yatindrarao.rao@gmail.com>
2016-01-24 10:16:19 +0530

    Basic User Authentication v2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users 
+  devise_for :users,
+    :path => '',
+    :path_names => {
+      :sign_in => 'login',
+      :sign_out => 'logout'
+    }
   		
   resources :pins
   root 'pins#index'
+    
 end
